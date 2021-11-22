3b3fa180d717f5310ef187c24d650186a7846376
TheAxZim <azeem.ilyas@mercari.com>
2021-11-23 00:12:23 +0900

    fix apostrophe in 404 page lint error

--- pages/404.js
+++ pages/404.js
@@ -8,11 +8,11 @@ export default function Custom404(){
     <MainLayout>
       <PageMeta 
           title="404 - Page Not Found"
-          description="You&apos;ve reached a page that may have been deleted or perhaps it never existed in the first place."
+          description="You have reached a page that may have been deleted or perhaps it never existed in the first place."
           url={Config.pageMeta.notFound.url}
       />
       <h1>404</h1>
-      <h2>You've reached a page that may have been deleted or perhaps it never existed in the first place.</h2>
+      <h2>You&apos;ve reached a page that may have been deleted or perhaps it never existed in the first place.</h2>
       <p><Link href="/">Click here to go home</Link></p>
     </MainLayout>
   )
