9f3afd1decbf26c05c7cac9b71146507ac405064
TheAxZim <azeem.ilyas@mercari.com>
2021-11-23 00:48:56 +0900

    test2

--- utils/ContentfulApi.js
+++ utils/ContentfulApi.js
@@ -415,6 +415,9 @@ export default class ContentfulApi {
       }
 
       static async callContentful(query, variables = {}, options = defaultOptions) {
+        console.log(`Contentful API SpaceID: ${process.env.CONTENTFUL_SPACE_ID}`)
+        console.log(`Contentful API Token: ${CONTENTFUL_ACCESS_TOKEN}`)
+        console.log(`Contentful Preview Token: ${process.env.NEXT_PUBLIC_CONTENTFUL_PREVIEW_ACCESS_TOKEN}`)
         const fetchUrl = `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`;
     
         const accessToken = options.preview
